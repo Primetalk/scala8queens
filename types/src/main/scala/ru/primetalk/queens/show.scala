63e39b427ac5ef5d8cbbea2d57fdb0d06db6acb0
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2023-03-27 23:19:06 +0300

    Type-level implementation

--- types/src/main/scala/ru/primetalk/queens/show.scala
+++ types/src/main/scala/ru/primetalk/queens/show.scala
@@ -15,7 +15,8 @@ sealed trait Render[T]:
 given renderInt[T](using t: ToInt[T]): Render[T] with
   def render: String = t.toInt.toString()
 
-given renderQueen[x,y](using x: ToInt[x], y: ToInt[y]):Render[Queen[x,y]] with
+given renderQueen[x, y](using x: ToInt[x], y: ToInt[y]): Render[Queen[x, y]]
+  with
   def render: String = s"♕(${x.toInt}, ${y.toInt})"
 
 sealed trait ToStringList[T]:
@@ -24,7 +25,10 @@ sealed trait ToStringList[T]:
 given ToStringList[Nil] with
   def toStringList: List[String] = scala.Nil
 
-given consToList[x, xs](using x: Render[x], xs: ToStringList[xs]): ToStringList[Cons[x, xs]] with
+given consToList[x, xs](using
+    x: Render[x],
+    xs: ToStringList[xs]
+): ToStringList[Cons[x, xs]] with
   def toStringList: List[String] = x.render :: xs.toStringList
 
 given renderList[xs](using xs: ToStringList[xs]): Render[xs] with
